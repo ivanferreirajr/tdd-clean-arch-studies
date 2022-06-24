a28359ed53d8f8f68d6458d552f8fa73f5b57f85
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2022-06-24 20:49:05 -0300

    chore: separate classes in different files

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,42 +1,5 @@
-class LoginRouter {
-    route(httpRequest) {
-        if (!httpRequest || !httpRequest.body) {
-            return HttpResponse.serverError();
-        }
-
-        const { email, password } = httpRequest.body;
-
-        if(!email) {
-            return HttpResponse.badRequest('email');
-        }
-
-        if(!password) {
-            return HttpResponse.badRequest('password');
-        }
-    }
-}
-
-class HttpResponse {
-    static badRequest(paramName) {
-        return {
-            statusCode: 400,
-            body: new MissingParamsError(paramName)
-        }
-    }
-
-    static serverError() {
-        return {
-            statusCode: 500
-        }
-    }
-}
-
-class MissingParamsError extends Error {
-    constructor(paramName) {
-        super(`Missing param: ${paramName}`);
-        this.name = 'MissingParamsError';
-    }
-}
+const LoginRouter = require('./login-router');
+const MissingParamsError = require('../helpers/missing-param-error');
 
 describe('Login Router', () => {
     test('Should return 400 if no email is provided', () => {
