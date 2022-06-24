2596c282795095e8170454569fc3915cbc45b795
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2022-06-24 20:26:05 -0300

    feat( loginRouter): ensure returns 500 if no httpRequest is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,13 @@
 class LoginRouter {
     route(httpRequest) {
+        if (!httpRequest || !httpRequest.body) {
+            return {
+                statusCode: 500
+            } 
+        }
+
         const { email, password } = httpRequest.body;
+
         if(!email || !password) {
             return {
                 statusCode: 400
@@ -17,8 +24,8 @@ describe('Login Router', () => {
                 password: 'password'
             }
         };
-
         const httpResponse = sut.route(httpRequest);
+
         expect(httpResponse.statusCode).toBe(400);
     });
 
@@ -29,8 +36,20 @@ describe('Login Router', () => {
                 email: 'email@mail.com'
             }
         };
-
         const httpResponse = sut.route(httpRequest);
+
         expect(httpResponse.statusCode).toBe(400);
+    })
+
+    test('Should return 500 if no httpRequest is provided', () => {
+        const sut = new LoginRouter();
+        const httpResponse = sut.route();
+        expect(httpResponse.statusCode).toBe(500);
+    })
+
+    test('Should return 500 if no body is provided', () => {
+        const sut = new LoginRouter();
+        const httpResponse = sut.route({});
+        expect(httpResponse.statusCode).toBe(500);
     })
 });
