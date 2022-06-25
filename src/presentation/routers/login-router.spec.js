087219a26ced2347dc1a56c8adf8583dcc33508a
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2022-06-24 21:10:11 -0300

    feat: ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,13 +1,25 @@
 const LoginRouter = require('./login-router');
 const MissingParamsError = require('../helpers/missing-param-error');
 
-const makeSup = () => {
-    return new LoginRouter();
+const makeSut = () => {
+    class AuthUseCaseSpy {
+        auth(email, password) {
+            this.email = email;
+            this.password = password;
+        }
+    }
+    const authUseCase = new AuthUseCaseSpy();
+    const sut = new LoginRouter(authUseCase);
+
+    return {
+        sut,
+        authUseCase
+    }
 }
 
 describe('Login Router', () => {
     test('Should return 400 if no email is provided', () => {
-        const sut = makeSup();
+        const { sut } = makeSut();
         const httpRequest = {
             body: { 
                 password: 'password'
@@ -20,7 +32,7 @@ describe('Login Router', () => {
     });
 
     test('Should return 400 if no password is provided', () => {
-        const sut = makeSup();
+        const { sut } = makeSut();
         const httpRequest = {
             body: { 
                 email: 'email@mail.com'
@@ -33,14 +45,27 @@ describe('Login Router', () => {
     })
 
     test('Should return 500 if no httpRequest is provided', () => {
-        const sut = makeSup();
+        const { sut } = makeSut();
         const httpResponse = sut.route();
         expect(httpResponse.statusCode).toBe(500);
     })
 
     test('Should return 500 if no body is provided', () => {
-        const sut = makeSup();
+        const { sut } = makeSut();
         const httpResponse = sut.route({});
         expect(httpResponse.statusCode).toBe(500);
+    })
+
+    test('Should call AuthUseCaseSpy with correct params', () => {
+        const { sut,authUseCase } = makeSut();
+        const httpRequest = {
+            body: {
+                email: 'test@example.com',
+                password: 'test'
+            }
+        }
+        sut.route(httpRequest);
+        expect(authUseCase.email).toBe(httpRequest.body.email);
+        expect(authUseCase.password).toBe(httpRequest.body.password);
     })
 });
