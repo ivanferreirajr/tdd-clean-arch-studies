d8fbf3b8d969602b8c943a425c13a91cac483e8d
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2022-06-24 21:15:26 -0300

    feat: ensure LoginRouter retuns 401 when invalid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -68,4 +68,16 @@ describe('Login Router', () => {
         expect(authUseCase.email).toBe(httpRequest.body.email);
         expect(authUseCase.password).toBe(httpRequest.body.password);
     })
+
+    test('Should return 401 with invalid credentials are provided', () => {
+        const { sut,authUseCase } = makeSut();
+        const httpRequest = {
+            body: {
+                email: 'invalid_test@example.com',
+                password: 'invalid_test'
+            }
+        }
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(401);
+    })
 });
